f57f27d571f1454a70b32165a2d1dff0b18bd0d2
Doosuur14 <dorisfaki@gmail.com>
2024-03-19 15:03:38 +0300

    Created HomeScreen and LoginScreen using snapkit for autolayout constraints.

---
+++ NavigateU/NavigateU/LoginScreen/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  NavigateU
+//
+//  Created by Faki Doosuur Doris on 16.03.2024.
+//
+
+import Foundation
---
+++ NavigateU/NavigateU/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  NavigateU
+//
+//  Created by Faki Doosuur Doris on 16.03.2024.
+//
+
+import Foundation
